15c5187c28fbfc2887ed875984d63967dc285f56
Stoumfou <alexis.pambourg@gmail.com>
2016-02-23 11:08:41 +0100

    Localization Traduction

---
+++ src/main/java/com/ece/ing4/bomberman/ressources/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Commande pour créer un jar
+ * Localisation et traduction
+ */
+/**
+ * @author Alexis
+ *
+ */
+package com.ece.ing4.bomberman.ressources;
---
+++ src/test/java/com/ece/ing4/bomberman/test/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Alexis
+ *
+ */
+package com.ece.ing4.bomberman.test;
